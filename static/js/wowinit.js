76e3560f47ee29363f56d5576bcef289c59a2937
i-apples <575404716@qq.com>
2018-11-07 16:37:10 +0800

    常规更新 11.7 新增页面 项目列表 赞助-项目介绍 赞助-收支明细

---
+++ static/js/wowinit.js
@@ -0,0 +1,11 @@
+//wowJs
+wow = new WOW(
+    {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       10,          // default
+        mobile:       true,       // default
+        live:         true        // default
+    }
+)
+wow.init();
